2e3c421cb775edd58c4e58b46552ea1725ac1f09
meduna femi <femimeduna@gmail.com>
2022-10-23 02:16:50 -0600

    form input styled component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,21 +1,18 @@
-import './form-input.styles.scss'
+import { Group, Input, FormInputLabel } from './form-input.styles'
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className='group'>
-      <input className='form-input' {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {
         label && (
-          <label
-            className={
-              `${otherProps.value.length ? 'shrink' : ''} form-input-label`
-            }>
+          <FormInputLabel shrink={otherProps.value.length} >
             {label}
-          </label>
+          </FormInputLabel>
         )
       }
 
-    </div>
+    </Group>
 
   )
 }
